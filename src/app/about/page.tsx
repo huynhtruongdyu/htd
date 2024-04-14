45d7f47f3a143a0b4624281ff491487410b9bc0e
Duy Huynh <huynhtruongdyu@gmail.com>
2024-04-14 12:10:23 +0700

    styles: add custom scrollbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Page = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quo,
+        perferendis nostrum eveniet similique minima, nihil pariatur
+        consequuntur rem odit obcaecati hic temporibus! Fugiat voluptate
+        reprehenderit dolores eius totam ab.
+      </p>
+    </div>
+  );
 };
 
 export default Page;
